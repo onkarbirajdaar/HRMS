6d9f037cb687c28bc47807d554f58d0d52fa6030
onkarbirajdaar <onkarbirajdar098@gmail.com>
2026-06-21 22:22:42 +0530

    Added attendance list page

--- employees/urls.py
+++ employees/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import dashboard, delete_employee, edit_employee, employee_add, employee_list
+from .views import attendance_list, dashboard, delete_employee, edit_employee, employee_add, employee_list
 
 urlpatterns = [
     path('', employee_list, name='employee_list'),
     path('add/', employee_add, name='employee_add'),
     path('edit/<int:id>', edit_employee, name='edit_employee'),
     path('delete/<int:id>', delete_employee, name='delete_employee'),
-    path('dashboard/', dashboard, name='dashboard')
+    path('dashboard/', dashboard, name='dashboard'),
+    path('attendance/', attendance_list, name='attendance_list'),
 ]
